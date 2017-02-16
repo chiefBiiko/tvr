c9a9aa25e1f2d17d97739b2bcfe69e3dad37f75b
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-02-16 20:21:52 +0100

    made arg passing more transparent

--- tvr.R
+++ tvr.R
@@ -23,6 +23,9 @@
 #   # add tasks
 #   tvr_add(c('new task', 'foo task'), rep(Sys.Date(), 2), rep(Sys.Date() + 1, 2))
 #   tvr_rm(1:2)  # remove tasks
+#   # working in a team
+#   tvr_team()  # pull and view ur team`s data
+#   tvr_push()  # update ur team's remote store with ur own data`
 
 lapply(list('jsonlite', 'timevis'), function(p) {
   if (!p %in% installed.packages()) install.packages(p)
@@ -39,52 +42,57 @@ TVR$ID <- sapply(list(TVR$NAME), function(n) {
 if (!dir.exists(file.path(.libPaths()[1], 'tvr'))) dir.create(file.path(.libPaths()[1], 'tvr'))
 TVR$DATA <- file.path(.libPaths()[1], 'tvr', 'tvr.Rda')  # path 2 ur own data
 
-tvr <- function() {
+tvr <- function(data=TVR$DATA) {
   # Loads and renders ur tvr data.
+  # @param {character} data Path 2 ur personal tvr data
   # @return {Object.<htmlwidgets>} Interactive timevis plot
-  stopifnot(nchar(TVR$DATA) > 0)
-  if (file.exists(TVR$DATA)) {
-    load(TVR$DATA)
+  stopifnot(nchar(data) > 0)
+  if (file.exists(data)) {
+    load(data)
   } else {
     tvr.data <- data.frame(id=NULL, content=NULL, start=NULL, end=NULL, group=NULL)
-    save(tvr.data, file=TVR$DATA)
+    save(tvr.data, file=data)
   }
   print.data.frame(tvr.data)
   return(timevis::timevis(tvr.data))
 }
 
-tvr_add <- function(content=NULL, start=NULL, end=NULL) {
+tvr_add <- function(content=NULL, start=NULL, end=NULL, data=TVR$DATA, id=TVR$ID) {
   # Adds tasks 2 ur tvr data and renders a new plot.
   # @param {character} content Vector of task descriptions
   # @param {Date} start Vector of Sys.Date() values
   # @param {Date} end Vector of Sys.Date() values
+  # @param {character} data Path 2 ur personal tvr data
+  # @param {integer} id Ur personal identifier
   # @return {Object.<htmlwidgets>} Interactive timevis plot
-  stopifnot(!missing(content), !missing(start), !missing(end))
-  if (file.exists(TVR$DATA)) {
-    load(TVR$DATA)
+  stopifnot(!missing(content), !missing(start), !missing(end),
+            nchar(data) > 0, is.integer(id))
+  if (file.exists(data)) {
+    load(data)
   } else {
     tvr.data <- data.frame(id=NULL, content=NULL, start=NULL, end=NULL, group=NULL)
   }
   new <- data.frame(id=double(length(content)), content=as.character(content),
                     start=format(start, '%Y-%m-%d'), end=format(end, '%Y-%m-%d'),
-                    group=rep(TVR$ID, length(content)))
+                    group=rep(id, length(content)))
   tvr.data <- rbind.data.frame(tvr.data, new, make.row.names=F, stringsAsFactors=F)
   tvr.data$id <- 1:nrow(tvr.data)  # reassigning ids here
-  save(tvr.data, file=TVR$DATA)
+  save(tvr.data, file=data)
   print.data.frame(tvr.data)
   return(timevis::timevis(tvr.data))
 }
 
-tvr_rm <- function(id=NULL) {
+tvr_rm <- function(id=NULL, data=TVR$DATA) {
   # Removes tasks from ur tvr data and renders a new plot.
   # @param {double} id Vector of task identifier/s
+  # @param {character} data Path 2 ur personal tvr data
   # @return {Object.<htmlwidgets>} Interactive timevis plot
-  stopifnot(file.exists(TVR$DATA), !missing(id))
-  load(TVR$DATA)
+  stopifnot(file.exists(data), !missing(id))
+  load(data)
   tvr.data <- tvr.data[!tvr.data$id %in% id, ]
   if (nrow(tvr.data) > 0) tvr.data$id <- 1:nrow(tvr.data)
   rownames(tvr.data) <- NULL
-  save(tvr.data, file=TVR$DATA)
+  save(tvr.data, file=data)
   print.data.frame(tvr.data)
   return(timevis::timevis(tvr.data))
 }
--- tvr_team.R
+++ tvr_team.R
@@ -14,15 +14,17 @@ TVRT$ID <- sapply(list(TVR$NAME), function(n) {
 
 TVRT$DATA <- file.path(.libPaths()[1], 'tvr', 'tvr.Rda')
 
-tvr_push <- function(name=TVRT$NAME, store_id=TVRT$STORE_ID) {
+tvr_push <- function(name=TVRT$NAME, store_id=TVRT$STORE_ID, data=TVRT$DATA, id=TVRT$ID) {
   # Update and view ur team's remote store
   # @param {character} name Ur name
   # @param {character} store_id Ur team's remote store identifier
+  # @param {character} data Path 2 ur personal tvr data
+  # @param {integer} id Ur personal identifier
   # @return {Object.<htmlwidgets>} Interactive timevis plot
-  stopifnot(nchar(name) > 0, nchar(store_id) > 0, file.exists(TVRT$DATA))
-  load(TVRT$DATA)
+  stopifnot(nchar(name) > 0, nchar(store_id) > 0, file.exists(data), is.numeric(id))
+  load(data)
   tvr.team <- jsonlite::fromJSON(paste0('https://api.myjson.com/bins/', store_id))
-  tvr.team$tsks <- tvr.team$tsks[tvr.team$tsks$group != TVRT$ID, ]  # cutting b4 merging
+  tvr.team$tsks <- tvr.team$tsks[tvr.team$tsks$group != id, ]  # cutting b4 merging
   tvr.team$tsks <- rbind.data.frame(tvr.team$tsks, tvr.data, make.row.names=F, stringsAsFactors=F)
   if (nrow(tvr.team$tsks) > 0) tvr.team$tsks$id <- 1:nrow(tvr.team$tsks)  # reassigning ids here
   re <- httr::PUT(paste0('https://api.myjson.com/bins/', store_id), body=tvr.team, encode='json')
