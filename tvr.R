e0f19ba93d95ca116a22e3b669529f4d2baf5604
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-02-16 13:24:48 +0100

    hooked my team up

--- tvr.R
+++ tvr.R
@@ -15,7 +15,7 @@
 #   @column {character} content Task description
 #   @column {Date} start Task start date
 #   @column {Date} end Task end date
-#   tvr.data is saved as ur personal task store @ tvr_DATA
+#   tvr.data is saved as ur personal task store @ TVR$DATA
 #
 # Usage
 #   tvr()  # view ur tasks
@@ -23,20 +23,27 @@
 #   tvr_add(c('new task', 'foo task'), rep(Sys.Date(), 2), rep(Sys.Date() + 1, 2))
 #   tvr_rm(1:2)  # remove tasks
 
+TVR <- list()
+TVR$NAME <- 'Biiko'  # 'Balou', 'Christian
+TVR$STORE_ID <- '1h2msv'
+TVR$ID <- sapply(list(TVR$NAME), function(n) {  # ur personal ID
+  hash <- jsonlite::fromJSON(paste0('https://api.myjson.com/bins/', TVR$STORE_ID))$hash
+  return(hash[hash$content == TVR$NAME, 'id'])
+}) 
+
 if (!dir.exists(file.path(.libPaths()[1], 'tvr'))) dir.create(file.path(.libPaths()[1], 'tvr'))
-tvr_DATA <- file.path(.libPaths()[1], 'tvr', 'tvr.Rda')
+TVR$DATA <- file.path(.libPaths()[1], 'tvr', 'tvr.Rda')  # ur own data
 
 tvr <- function() {
   # Loads ur tvr data and returns an interactive timeplot of it.
   # @return {Object.<htmlwidgets>} Interactive timevis plot
   stopifnot(isTRUE(require('timevis')))
-  if (file.exists(tvr_DATA)) {
-    load(tvr_DATA)
+  if (file.exists(TVR$DATA)) {
+    load(TVR$DATA)
   } else {
-    tvr.data <- data.frame(id=NULL, content=NULL, start=NULL, end=NULL)
-    save(tvr.data, file=tvr_DATA)
+    tvr.data <- data.frame(id=NULL, content=NULL, start=NULL, end=NULL, group=NULL)
+    save(tvr.data, file=TVR$DATA)
   }
-  View(tvr.data)
   return(timevis::timevis(tvr.data))
 }
 
@@ -46,18 +53,18 @@ tvr_add <- function(content=NULL, start=NULL, end=NULL) {
   # @param {Date} start Vector of Sys.Date() values
   # @param {Date} end Vector of Sys.Date() values
   # @return {Object.<htmlwidgets>} Interactive timevis plot
-  stopifnot(isTRUE(require('timevis')),
-            !missing(content), !missing(start), !missing(end))
-  if (file.exists(tvr_DATA)) {
-    load(tvr_DATA)
+  stopifnot(isTRUE(require('timevis')), !missing(content), !missing(start), !missing(end))
+  if (file.exists(TVR$DATA)) {
+    load(TVR$DATA)
   } else {
-    tvr.data <- data.frame(id=NULL, content=NULL, start=NULL, end=NULL)
+    tvr.data <- data.frame(id=NULL, content=NULL, start=NULL, end=NULL, group=NULL)
   }
-  new <- data.frame(id=double(length(content)), content=content, start=start, end=end)
-  tvr.data <- rbind(tvr.data, new)
+  new <- data.frame(id=double(length(content)), content=content, start=start, end=end,
+                    group=rep(TVR$ID, length(content)))
+  tvr.data <- rbind.data.frame(tvr.data, new)
   tvr.data$id <- 1:nrow(tvr.data)  # reassigning ids here
   rownames(tvr.data) <- NULL
-  save(tvr.data, file=tvr_DATA)
+  save(tvr.data, file=TVR$DATA)
   View(tvr.data)
   return(timevis::timevis(tvr.data))
 }
@@ -66,12 +73,12 @@ tvr_rm <- function(id=NULL) {
   # Removes tasks from ur tvr data and renders a new plot.
   # @param {double} id Vector of task identifier/s
   # @return {Object.<htmlwidgets>} Interactive timevis plot
-  stopifnot(isTRUE(require('timevis')), file.exists(tvr_DATA), !missing(id))
-  load(tvr_DATA)
+  stopifnot(isTRUE(require('timevis')), file.exists(TVR$DATA), !missing(id))
+  load(TVR$DATA)
   tvr.data <- tvr.data[!tvr.data$id %in% id, ]
   if (nrow(tvr.data) > 0) tvr.data$id <- 1:nrow(tvr.data)
   rownames(tvr.data) <- NULL
-  save(tvr.data, file=tvr_DATA)
+  save(tvr.data, file=TVR$DATA)
   View(tvr.data)
   return(timevis::timevis(tvr.data))
 }
--- tvr_team.R
+++ tvr_team.R
@@ -1,26 +1,34 @@
 # tvr_team
 
-NAME <- 'Biiko'  # 'Balou'
-STORE_ID <- '1h2msv'
-tvr_DATA <- file.path(.libPaths()[1], 'tvr', 'tvr.Rda')
-tvr_TEAM <- file.path(.libPaths()[1], 'tvr', 'tvr_team.Rda')
+TVRT <- list()
+TVRT$NAME <- 'Biiko'  # 'Balou', 'Christian
+TVRT$STORE_ID <- '1h2msv'
+TVRT$DATA <- file.path(.libPaths()[1], 'tvr', 'tvr.Rda')
 
-tvr_push <- function (name=NAME, store_id=STORE_ID) {
+tvr_push <- function(name=TVRT$NAME, store_id=TVRT$STORE_ID) {
   # Update and view ur team's remote store
-  stopifnot(isTRUE(require('timevis')), nchar(name) > 0, nchar(store_id) > 0,
-            file.exists(tvr_DATA))
-  load(tvr_DATA)
+  # @param {character} name Ur name
+  # @param {character} store_id Ur team's remote store identifier
+  # @return {Object.<htmlwidgets>} Interactive timevis plot
+  stopifnot(isTRUE(require('jsonlite')), isTRUE(require('httr')), isTRUE(require('timevis')), 
+            nchar(name) > 0, nchar(store_id) > 0, file.exists(TVRT$DATA))
+  load(TVRT$DATA)
   tvr.team <- jsonlite::fromJSON(paste0('https://api.myjson.com/bins/', store_id))
-  save(tvr.team, file=tvr_TEAM)
-  # prepare 4 PUT ... aka merge tvr.data and tvr.team
-  # ... PUT
-  # show response data
+  tvr.team$tsks <- rbind.data.frame(tvr.team$tsks, tvr.data)  # merging
+  if (nrow(tvr.team$tsks) > 0) tvr.team$tsks$id <- 1:nrow(tvr.team$tsks)  # reassigning ids here
+  rownames(tvr.team$tsks) <- NULL
+  re <- httr::PUT(paste0('https://api.myjson.com/bins/', store_id), body=tvr.team, encode='json')
+  if (httr::status_code(re) != 200) stop('Upload error.')
+  tvr.team <- jsonlite::fromJSON(paste0('https://api.myjson.com/bins/', store_id))
+  View(tvr.team$tsks)
+  return(timevis::timevis(tvr.team$tsks, tvr.team$hash))
 }
 
-tvr_team <- function (store_id=STORE_ID) {
+tvr_team <- function(store_id=TVRT$STORE_ID) {
   # Pull and view ur team's remote store
-  stopifnot(isTRUE(require('timevis')), nchar(store_id) > 0)
+  # @param {character} store_id Ur team's remote store identifier
+  # @return {Object.<htmlwidgets>} Interactive timevis plot
+  stopifnot(isTRUE(require('jsonlite')), isTRUE(require('timevis')), nchar(store_id) > 0)
   tvr.team <- jsonlite::fromJSON(paste0('https://api.myjson.com/bins/', store_id))
-  save(tvr.team, file=tvr_TEAM)
-  return(timevis::timevis(tvr.team))
+  return(timevis::timevis(tvr.team$tsks, tvr.team$hash))
 }
